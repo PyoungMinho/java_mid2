496b7e1f0d4d483f4a835a570942f456104ee4b5
pyoung <pmmh9395@gmail.com>
2024-07-18 13:48:45 +0900

    java_mid2 / Hash - 자바의 hashCode()

--- src/collection/set/JavaHashCodeMain.java
+++ src/collection/set/JavaHashCodeMain.java
@@ -22,14 +22,13 @@ public class JavaHashCodeMain {
 
         //hashCode는 마이너스 값이 들어올 수 있다.
         System.out.println("-1.hashCode = " + Integer.valueOf(-1).hashCode());
-        
+
         //둘은 같을까 다를까?, 인스턴스는 다르지만, equals는 같다.
         Member member1 = new Member("idA");
         Member member2 = new Member("idA");
         //equals, hashCode를 오버라이딩 하지 않은 경우와, 한 경우를 비교
         System.out.println("(member1 == member2) = " + (member1 == member2));
-        System.out.println("member1 equals member2 = " +
-                member1.equals(member2));
+        System.out.println("member1 equals member2 = " + member1.equals(member2));
         System.out.println("member1.hashCode() = " + member1.hashCode());
         System.out.println("member2.hashCode() = " + member2.hashCode());
     }
