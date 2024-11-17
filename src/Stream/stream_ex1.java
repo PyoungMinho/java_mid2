ca30dca7bcd7b5a44a5d6f8cf91a08ad66fc897a
Pyoung MinHo <pmmh9395@gmail.com>
2024-11-17 14:05:50 +0900

    lamda start

--- src/Stream/stream_ex1.java
+++ src/Stream/stream_ex1.java
@@ -1,8 +1,29 @@
 package Stream;
 
+import java.util.*;
+
 public class stream_ex1 {
     public static void main(String[] args) {
         // 람다 Stream 을 공부하기 위한 공간
+        List<String> names = Arrays.asList("Alice","b","c","d","e");
+
+        List<String> filterNames =  new ArrayList<>();
+        for (String name : names) {
+            if(name.startsWith("A") && name.length() >= 4){
+                filterNames.add(name);
+            }
+        }
+
+        Collections.sort(filterNames);
+        for (String filterName : filterNames) {
+            System.out.println("name is : "+ filterName);
+        }
+
+        // =======> 람다로변경
+        names.stream()
+                .filter(name -> name.startsWith("A") && name.length() >= 4)
+                .sorted()
+                .forEach(System.out::println);
 
     }
 }
