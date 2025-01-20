387c3701a6edd8097e1c76c8b50ddac32c32b378
Pyoung MinHo <pmmh9395@gmail.com>
2025-01-20 16:54:47 +0900

    거스름돈

--- src/collection/Inheritance/MainInheritance.java
+++ src/collection/Inheritance/MainInheritance.java
@@ -39,9 +39,9 @@ public class MainInheritance {
         int m = 4620;
 
         int a = m /1000;
-        int b = m % 1000 /500;
+        int b = m % 1000 / 500;
         int c = m % 500 / 100;
-        int d = m /1000;
+        int d = m % 100 / 10 ;
 
         System.out.println(a);
         System.out.println(b);
