8f38d391b86d22916a486ed0ddfd34dc95e5b816
Pyoung MinHo <pmmh9395@gmail.com>
2025-01-21 16:46:55 +0900

    maxVal 재귀 , 자바식

--- src/collection/Inheritance/MainInheritance.java
+++ src/collection/Inheritance/MainInheritance.java
@@ -48,7 +48,29 @@ public class MainInheritance {
         System.out.println(c);
         System.out.println(d);
 
+        int[] myArr = {8, 1, 2, 9, 1};
+        int size = myArr.length; // 배열 크기 계산
+
+        System.out.println("배열 최대값: " + maxVal(myArr, size)); // 결과 출력
+        System.out.println("배열 최대값2: " + maxVal2(myArr)); // 결과 출력
+
+
+    }
+    public static int maxVal(int[] arr, int size) {
+        if (size == 1) {
+            return arr[0]; // 배열 크기가 1인 경우, 첫 번째 값 반환
+        } else {
+            int max = maxVal(arr, size - 1); // 재귀 호출로 나머지 배열의 최대값 찾기
+            return Math.max(arr[size - 1], max); // 현재 값과 이전 최대값 비교 후 반환
+        }
     }
 
+    public static int maxVal2(int[] arr) {
+        int max = arr[0];
+        for (int j : arr) {
+            max = Math.max(max, j);
+        }
+        return max;
+    }
 
 }
