d704bcab33da0f83eb71ed6aa58b6036b30fc4d5
Pyoung MinHo <pmmh9395@gmail.com>
2025-01-23 11:33:17 +0900

    소수판별+ 에라토스테네스의 체

--- src/collection/Inheritance/MainInheritance.java
+++ src/collection/Inheritance/MainInheritance.java
@@ -55,8 +55,10 @@ public class MainInheritance {
         System.out.println("배열 최대값2: " + maxVal2(myArr)); // 결과 출력
 
 
-        int solution = solution(10);
-        System.out.println("answer : "+ solution);
+        int solution1 = solution(10);
+        int solution2 = solution2(10);
+        System.out.println("answer : "+ solution1);
+        System.out.println("answer : "+ solution2);
 
     }
     public static int maxVal(int[] arr, int size) {
@@ -96,6 +98,33 @@ public class MainInheritance {
         return answer;
 
     }
+
+
+        public static int solution2(int n) { // 메서드 선언
+            if (n < 2) return 0; // n이 2보다 작으면 소수가 없음
+
+            boolean[] isPrime = new boolean[n + 1]; // 소수 여부를 저장하는 배열
+            for (int i = 2; i <= n; i++) {
+                isPrime[i] = true; // 처음에는 모든 숫자를 소수로 가정
+            }
+
+            for (int i = 2; i * i <= n; i++) { // i의 배수를 지움
+                if (isPrime[i]) { // i가 소수인 경우
+                    for (int j = i * i; j <= n; j += i) { // i의 배수를 모두 false로 설정
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            int answer = 0; // 소수의 개수 카운트
+            for (int i = 2; i <= n; i++) {
+                if (isPrime[i]) {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
 
 
 
