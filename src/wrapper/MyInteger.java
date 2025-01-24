bf7c5f37183648f5dd9240d2ff1a6ae9ac1189b0
Pyoung MinHo <pmmh9395@gmail.com>
2025-01-24 13:44:57 +0900

    JAVA Wrapper class

---
+++ src/wrapper/MyInteger.java
@@ -0,0 +1,25 @@
+package wrapper;
+
+public class MyInteger {
+
+    private final int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int compareTo(int target) {
+        if (this.value < target) {return -1;}
+        else if (this.value > target) {return 1;}
+        else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.value);
+    }
+}
