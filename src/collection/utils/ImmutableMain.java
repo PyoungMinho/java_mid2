24be8a90bae173103e7ea99a76c27f1c2302c034
Pyoung MinHo <pmmh9395@gmail.com>
2025-02-13 13:21:39 +0900

    실기 22년 3회 코드해석

--- src/collection/utils/ImmutableMain.java
+++ src/collection/utils/ImmutableMain.java
@@ -19,5 +19,29 @@ public class ImmutableMain {
     //가변 -> 불변리스트로
     List<Integer> unmodifiableList = Collections.unmodifiableList(mutableList);
     System.out.println(" unmodifiableList: " + unmodifiableList.getClass());
+
+
+
+    // 예제
+    int n;
+    int k;
+    int s=0;
+    int el = 0;
+
+    for(n =6; n<=30; n++){
+      s = 0;
+      k = n/2;
+      for (int j=1; j<=k; j++){
+        if(n % j ==0 ) s = s+j;
+      }
+      if(s==n) el++;
+    }
+    System.out.println("el: " + el);
+
+    int a=0;
+    for (int i =1; i<999; i++){
+      if (i%3 == 0 && i % 2 !=0) a= i; // 3으로 나눠지지만 2로 안나눠 지는 마지막 숫자 993
+    }
+    System.out.println("a: " + a);
   }
 }
