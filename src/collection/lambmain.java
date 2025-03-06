dd38a6f2854c4670ed5d18c66399dcabfebbd0c9
minho <pmmh9395@gmail.com>
2025-03-06 17:02:01 +0900

    정처기 java 기출 문제 Done

--- src/collection/lambmain.java
+++ src/collection/lambmain.java
@@ -17,3 +17,20 @@ public class lambmain {
     int sum(int a, int b);
   }
 } // 인터페이스를 람다로 재정의하기 
+
+/*
+
+def cnt_occ(s,sub):
+cnt = 0
+  for i in range(len(s) - len(sub) +1):
+  if s[i:i+len(sub)] == sub:
+cnt += 1
+  return cnt
+
+  a = "abdcavcaavca"
+p1 ="ab"
+p2 = "ca"
+
+out = f"ab{cnt_occ(a,p1)}ca{cnt_occ(a,p2)}"
+print(out)
+*/
